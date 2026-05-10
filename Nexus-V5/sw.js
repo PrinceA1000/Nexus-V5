8f54e985ad068b092c85b3f690f6ae446dd8c9f4
PrinceA1000 <137448619+PrinceA1000@users.noreply.github.com>
2026-05-10 19:00:30 +0000

    change the manifest file to reflect the new location of the files.

--- Nexus-V5/sw.js
+++ Nexus-V5/sw.js
@@ -1,14 +1,17 @@
 const CACHE_NAME = 'crown-nexus-v5';
+
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(['./index.html', './manifest.json']))
   );
 });
+
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
   );
 });
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(res => res || fetch(event.request).catch(() => new Response('Offline', { status: 503 })))
